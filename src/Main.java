c4a4279977310502ff5bb5ad317e406c8dc6d85e
Antoine Fivet <antoine.fivet@gmail.com>
2023-05-06 14:55:27 +0200

    Third test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Second test");
-        System.out.println("See if it works well");
+        System.out.println("See if it works");
     }
 }
