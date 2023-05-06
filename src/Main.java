653510c354f4a6a28eb52489520f410033ea7f76
Antoine Fivet <antoine.fivet@gmail.com>
2023-05-06 15:45:18 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Second test");
         System.out.println("See if it works");
-        System.out.println("anas");
+        System.out.println("fivet");
     }
 }
