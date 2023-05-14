82fc858ab664bfce5815caa30e7d966ae816072f
Toon <antoine_collard@outlook.com>
2023-05-14 18:36:45 +0200

    - Ajout de la classe model discountMemberAgeRangeModel pour pouvoir avoir des objets qui seront ajouté dans la classe findDiscountMemberAgeRangeModel
    - Ajout de la classe model findDiscountMemberAgeRangeModel dans la couche view pour qu'un Jtable puisse l'afficher

---
+++ src/viewPackage/findDiscountMemberAgeRangeModel.java
@@ -0,0 +1,52 @@
+package viewPackage;
+
+import modelPackage.discountMemberAgeRangeModel;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+
+public class findDiscountMemberAgeRangeModel extends AbstractTableModel {
+    private ArrayList<String> columnNames;
+    private ArrayList<discountMemberAgeRangeModel> contents;
+
+    public findDiscountMemberAgeRangeModel(ArrayList<discountMemberAgeRangeModel> members) {
+        columnNames = new ArrayList<>();
+        columnNames.add("First name");
+        columnNames.add("Last name");
+        columnNames.add("Discount");
+        columnNames.add("Client number");
+        setContents(members);
+    }
+
+    private void setContents(ArrayList<discountMemberAgeRangeModel> members) {
+        contents = members;
+    }
+
+    public int getColumnCount( ) { return columnNames.size( ); }
+    public int getRowCount( ) { return contents.size( ); }
+    public String getColumnName(int column) { return columnNames.get(column); }
+
+    public Object getValueAt (int row, int column) {
+        discountMemberAgeRangeModel member = contents.get(row);
+        switch(column) {
+            case 0 : return member.getFirstName();
+            case 1 : return member.getLastName();
+            case 2: return member.getDiscount();
+            case 3: return member.getClientNumber();
+            default : return null;
+        }
+    }
+
+    public Class getColumnClass (int column) {
+        Class c;
+
+        switch (column) {
+            case 0: c = String.class; break;
+            case 1: c = String.class; break;
+            case 2: c = Integer.class; break;
+            case 3: c = Integer.class; break;
+            default: c = String.class;
+        }
+        return c;
+    }
+}
