2d065916e5e68493e04747b5ba1c2bd0fe69cdb6
Antoine Fivet <antoine.fivet@gmail.com>
2023-05-14 23:17:48 +0200

    Changement noms de variables d'instance Début vérification données entrées par l'utilisateur

--- src/viewPackage/RegistrationForm.java
+++ src/viewPackage/RegistrationForm.java
@@ -14,32 +14,36 @@ public class RegistrationForm extends JPanel{
     private JButton validationButton;
     private JButton reinitialisationButton;
 
-    private JTextField nationalNumber;
-    private JTextField lastName;
-    private JTextField firstName;
-    private JDateChooser birthDate;
-    private JTextField phoneNumber;
-    private JComboBox<String> gender;
-    private JTextField emailAdress;
-    private JTextField street;
-    private JTextField streetNumber;
-    private JTextField locality;
-    private JTextField postalCode;
-    private JCheckBox newsletter;
+    private JTextField nationalNumberField;
+    private JTextField lastNameField;
+    private JTextField firstNameField;
+    private JDateChooser birthDateField;
+    private JTextField phoneNumberField;
+    private JComboBox<String> genderComboBox;
+    private JTextField emailAdressField;
+    private JTextField streetField;
+    private JTextField streetNumberField;
+    private JTextField localityField;
+    private JTextField postalCodeField;
+    private JCheckBox newsletterField;
 
     public void resetForm(){
-        nationalNumber.setText("");
-        lastName.setText("");
-        firstName.setText("");
-        phoneNumber.setText("");
-        gender.setSelectedIndex(0);
-        emailAdress.setText("");
-        birthDate.setDate(null);
-        street.setText("");
-        streetNumber.setText("");
-        locality.setText("");
-        postalCode.setText("");
-        newsletter.setSelected(false);
+        nationalNumberField.setText("");
+        lastNameField.setText("");
+        firstNameField.setText("");
+        phoneNumberField.setText("");
+        genderComboBox.setSelectedIndex(0);
+        emailAdressField.setText("");
+        birthDateField.setDate(null);
+        streetField.setText("");
+        streetNumberField.setText("");
+        localityField.setText("");
+        postalCodeField.setText("");
+        newsletterField.setSelected(false);
+    }
+
+    public boolean validForm(){
+        return false;
     }
 
     public RegistrationForm(){
@@ -47,52 +51,52 @@ public class RegistrationForm extends JPanel{
         formPanel.setLayout(new GridLayout(0, 2, 10, 15));
 
         formPanel.add(new JLabel("National number"));
-        nationalNumber = new JTextField();
-        formPanel.add(nationalNumber);
+        nationalNumberField = new JTextField();
+        formPanel.add(nationalNumberField);
 
         formPanel.add(new JLabel("Last name"));
-        lastName = new JTextField();
-        formPanel.add(lastName);
+        lastNameField = new JTextField();
+        formPanel.add(lastNameField);
 
         formPanel.add(new JLabel("First name"));
-        firstName = new JTextField();
-        formPanel.add(firstName);
+        firstNameField = new JTextField();
+        formPanel.add(firstNameField);
 
         formPanel.add(new JLabel("Birth date"));
-        birthDate = new JDateChooser();
-        formPanel.add(birthDate);
+        birthDateField = new JDateChooser();
+        formPanel.add(birthDateField);
 
         formPanel.add(new JLabel("Phone Number"));
-        phoneNumber = new JTextField();
-        formPanel.add(phoneNumber);
+        phoneNumberField = new JTextField();
+        formPanel.add(phoneNumberField);
 
         formPanel.add(new JLabel("Gender"));
-        gender = new JComboBox<>();
-        gender.addItem("Male");
-        gender.addItem("Female");
-        gender.addItem("Other");
-        formPanel.add(gender);
+        genderComboBox = new JComboBox<>();
+        genderComboBox.addItem("Male");
+        genderComboBox.addItem("Female");
+        genderComboBox.addItem("Other");
+        formPanel.add(genderComboBox);
 
         formPanel.add(new JLabel("Email address"));
-        emailAdress = new JTextField();
-        formPanel.add(emailAdress);
+        emailAdressField = new JTextField();
+        formPanel.add(emailAdressField);
 
-        street = new JTextField();
-        streetNumber = new JTextField();
-        locality = new JTextField();
-        postalCode = new JTextField();
+        streetField = new JTextField();
+        streetNumberField = new JTextField();
+        localityField = new JTextField();
+        postalCodeField = new JTextField();
         formPanel.add(new JLabel("Street"));
-        formPanel.add(street);
+        formPanel.add(streetField);
         formPanel.add(new JLabel("Street number"));
-        formPanel.add(streetNumber);
+        formPanel.add(streetNumberField);
         formPanel.add(new JLabel("Locality"));
-        formPanel.add(locality);
+        formPanel.add(localityField);
         formPanel.add(new JLabel("Postal code"));
-        formPanel.add(postalCode);
+        formPanel.add(postalCodeField);
 
         formPanel.add(new JLabel("Do you want to subscribe to the newsletter?"));
-        newsletter = new JCheckBox();
-        formPanel.add(newsletter);
+        newsletterField = new JCheckBox();
+        formPanel.add(newsletterField);
 
         buttonsPanel = new JPanel();
 
@@ -105,8 +109,11 @@ public class RegistrationForm extends JPanel{
         reinitialisationButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                resetForm();
-                JOptionPane.showMessageDialog(null, "Form reset.", "Information", JOptionPane.INFORMATION_MESSAGE);
+                int response = JOptionPane.showConfirmDialog(null, "Are you sure you want to reset the form?", "Validation", JOptionPane.YES_NO_OPTION);
+                if(response == JOptionPane.YES_OPTION) {
+                    resetForm();
+                    JOptionPane.showMessageDialog(null, "Form reset.", "Information", JOptionPane.INFORMATION_MESSAGE);
+                }
             }
         });
 
@@ -115,7 +122,13 @@ public class RegistrationForm extends JPanel{
             public void actionPerformed(ActionEvent e) {
                 int response = JOptionPane.showConfirmDialog(null, "Are you sure you want to validate your registration?", "Validation", JOptionPane.YES_NO_OPTION);
                 if(response == JOptionPane.YES_OPTION){
-                    // VERIFIER LES DONNEES
+                    if(nationalNumberField.getText().length() == 11){
+                        int nationalNumber = Integer.parseInt(nationalNumberField.getText());
+                        JOptionPane.showMessageDialog(null,"Bon numéro","Bon",JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(null,"mauvais numéro","mauvais",JOptionPane.INFORMATION_MESSAGE);
+                    }
                     // SI ELLES SONT BONNES
                         // ENTRER LES DONNEES DANS LA BD
                         JOptionPane.showMessageDialog(null, "Registration validated.", "Information", JOptionPane.INFORMATION_MESSAGE);
