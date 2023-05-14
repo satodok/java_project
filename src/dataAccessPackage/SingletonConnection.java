9fb11b2b97f51e5a719170b34b2e4a3bb67ba966
Anas <anas1801@outlook.com>
2023-05-14 14:06:16 +0200

    -Creation du packageModel et de la classe MemberAddress. -Création de l'interface DataAccess.

--- src/dataAccessPackage/SingletonConnection.java
+++ src/dataAccessPackage/SingletonConnection.java
@@ -16,7 +16,7 @@ public class SingletonConnection {
             if (uniqueConnection == null) {
                 uniqueConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/libiavelo",
                         "root",
-                        "Haloreach89");
+                        "votremdp");
             }
         } catch (SQLException sqlException) {
             String message = "Erreur lors de la connexion à la base de données libiavelo.";
