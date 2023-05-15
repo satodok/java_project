50ca9aa65ab0460fba0318736f4b42d642497211
Antoine Fivet <antoine.fivet@gmail.com>
2023-05-15 16:03:44 +0200

    Correction MemberInformations

--- src/viewPackage/MemberInformations.java
+++ src/viewPackage/MemberInformations.java
@@ -17,25 +17,6 @@ public class MemberInformations extends JFrame{
     public MemberInformations(){
         super("My informations");
         this.setBounds(100,100,400,400);
-        setController(new ApplicationController());
-        nationalNumberInupt = JOptionPane.showInputDialog(null, "Please enter your national number");
-        nationalNumber = Integer.parseInt(nationalNumberInupt);
-        try{
-            MemberAddress memberAddress = controller.findMemberAdressByNationalNumber(nationalNumber);
-            MemberAddressModel model = new MemberAddressModel(memberAddress);
-            JTable table = new JTable(model);
-            JScrollPane scrollPane = new JScrollPane(table);
-            this.add(scrollPane);
-            setVisible(true);
-        }
-        catch (UnfoundResearchException unfoundResearchException){
-            JOptionPane.showMessageDialog(null, unfoundResearchException.getMessage(),
-                    "Erreur", JOptionPane.ERROR_MESSAGE);
-        }
-        catch(ConnectionException connectionException){
-            JOptionPane.showMessageDialog(null, connectionException.getMessage(),
-                    "Erreur", JOptionPane.ERROR_MESSAGE);
-        }
     }
     public void setController(ApplicationController controller) {
         this.controller = controller;
