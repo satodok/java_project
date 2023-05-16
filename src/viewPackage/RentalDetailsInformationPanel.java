ba64310ce4f2a8f3514a65e2cc17067b0d4faa28
Toon <antoine_collard@outlook.com>
2023-05-16 10:52:40 +0200

    -visuel de la 4ème recherche terminé

--- src/viewPackage/RentalDetailsInformationPanel.java
+++ src/viewPackage/RentalDetailsInformationPanel.java
@@ -7,6 +7,9 @@ import exceptionPackage.UnfoundResearchException;
 import modelPackage.RentalDetailsInformation;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
@@ -20,49 +23,50 @@ public class RentalDetailsInformationPanel extends JFrame {
 
     public RentalDetailsInformationPanel() {
         super("Research rental details in range");
-        this.setBounds(100, 100, 400, 400);
+        this.setBounds(100, 100, 600, 600);
         setController(new ApplicationController());
 
-        JButton searchButton = new JButton("Search");
+        JButton searchButton = new JButton("Search for range");
         panel.add(searchButton); // Ajoute le bouton de recherche au panneau
+        setVisible(true);
+        add(panel, BorderLayout.NORTH);
 
-        String startDateInput = JOptionPane.showInputDialog(null, "Enter the start date (yyyy-mm-dd):");
-        String endDateInput = JOptionPane.showInputDialog(null, "Enter the end date (yyyy-mm-dd):");
 
-        searchButton.addActionListener(e -> {
-            // Demande de startDate et endDate en utilisant JOptionPane
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Demande de startDate et endDate en utilisant JOptionPane
+                String startDateInput = JOptionPane.showInputDialog(null, "Enter the start date (yyyy-mm-dd):");
+                String endDateInput = JOptionPane.showInputDialog(null, "Enter the end date (yyyy-mm-dd):");
 
-            try {
-                // Conversion des dates en GregorianCalendar
-                startDate.setTime(Date.valueOf(startDateInput));
-                endDate.setTime(Date.valueOf(endDateInput));
+                try {
+                    // Conversion des dates en GregorianCalendar
+                    startDate.setTime(Date.valueOf(startDateInput));
+                    endDate.setTime(Date.valueOf(endDateInput));
 
-                ArrayList<RentalDetailsInformation> rentals = controller.findRentalDetailsFromDateRange(new java.sql.Date(startDate.getTimeInMillis()), new java.sql.Date(endDate.getTimeInMillis()));
-                RentalDetailsInformationModel model = new RentalDetailsInformationModel(rentals);
-                JTable table = new JTable(model);
-                JScrollPane scrollPane = new JScrollPane(table);
-                getContentPane().removeAll(); // Supprime les composants existants de la fenêtre
-                getContentPane().add(scrollPane); // Ajoute le tableau à la fenêtre
-                revalidate(); // Actualise l'affichage de la fenêtre
+                    ArrayList<RentalDetailsInformation> rentals = controller.findRentalDetailsFromDateRange(new java.sql.Date(startDate.getTimeInMillis()), new java.sql.Date(endDate.getTimeInMillis()));
+                    RentalDetailsInformationModel model = new RentalDetailsInformationModel(rentals);
 
-            } catch (UnfoundResearchException unfoundResearchException) {
-                JOptionPane.showMessageDialog(null, unfoundResearchException.getMessage(),
-                        "Erreur", JOptionPane.ERROR_MESSAGE);
-            } catch (ConnectionException connectionException) {
-                JOptionPane.showMessageDialog(null, connectionException.getMessage(),
-                        "Erreur", JOptionPane.ERROR_MESSAGE);
+                    JTable table = new JTable(model);
+                    JScrollPane scrollPane = new JScrollPane(table);
 
-            } catch (RentalDetailsException exception) {
-                JOptionPane.showMessageDialog(null, exception.getMessage(),
-                        "Erreur", JOptionPane.ERROR_MESSAGE);
+                    add(scrollPane, BorderLayout.SOUTH);
+                    setVisible(true);
+
+                } catch (UnfoundResearchException unfoundResearchException) {
+                    JOptionPane.showMessageDialog(null, unfoundResearchException.getMessage(),
+                            "Erreur", JOptionPane.ERROR_MESSAGE);
+                } catch (ConnectionException connectionException) {
+                    JOptionPane.showMessageDialog(null, connectionException.getMessage(),
+                            "Erreur", JOptionPane.ERROR_MESSAGE);
+
+                } catch (RentalDetailsException exception) {
+                    JOptionPane.showMessageDialog(null, exception.getMessage(),
+                            "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
             }
         });
 
-
-
-        getContentPane().add(panel); // Ajoute le panneau à la fenêtre
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
     }
 
     public void setController(ApplicationController controller) {
