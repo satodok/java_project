881d310558ef495741246f73a19bb57df2a37aad
Anas <anas1801@outlook.com>
2023-05-17 00:57:52 +0200

    -Creation du formulaire pour l'update de member.

--- src/viewPackage/SubscriptionFormPanel.java
+++ src/viewPackage/SubscriptionFormPanel.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class SubscriptionFormPanel extends JPanel {
 
     public SubscriptionFormPanel(){
-        super("Subscribe");
+
         this.setBounds(100,100,400,400);
     }
 }
